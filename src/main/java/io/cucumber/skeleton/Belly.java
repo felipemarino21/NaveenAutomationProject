8b8d950c4d2f8b668e8b6b190edeeb106d8c907f
cristian barahona <puescristian@hotmail.com>
2022-06-13 16:31:25 -0500

    first

---
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -0,0 +1,9 @@
+package io.cucumber.skeleton;
+
+import org.openqa.selenium.WebDriver;
+
+public class Belly {
+    public void eat(int cukes) {
+
+    }
+}
---
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -0,0 +1,16 @@
+package io.cucumber.skeleton;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+
+public class StepDefinitions {
+    @Given("I have {int} cukes in my belly")
+    public void I_have_cukes_in_my_belly(int cukes) {
+        Belly belly = new Belly();
+        belly.eat(cukes);
+        WebDriver driver;
+    }
+
+}
