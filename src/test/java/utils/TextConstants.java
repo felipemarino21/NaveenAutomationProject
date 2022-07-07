0608527e5a09f08e4e0ba68cf412fda27e082a65
felipemarino <luisfelipemarino@gmail.com>
2022-07-07 10:53:53 -0500

    final commit

--- src/test/java/utils/TextConstants.java
+++ src/test/java/utils/TextConstants.java
@@ -6,6 +6,6 @@ public class TextConstants {
     public static String AccountUrl = "https://naveenautomationlabs.com/opencart/index.php?route=account/account";
     public static String CreateConfirmation = "Your Account Has Been Created!";
     public static String EmailWarning = "Warning: E-Mail Address is already registered!";
-    
+
 
 }
